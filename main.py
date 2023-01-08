804767774a62824d7a374e53d361459610bcde67
Yohanes Gultom <yohanes.gultom@gmail.com>
2023-01-08 21:34:20 +0700

    fix copy command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import click
 import json
-import shutil
 import subprocess
 import os
 from urllib import request, error
@@ -50,7 +49,7 @@ def create_reverse_proxy(subdomain, domain, target_ip, target_port):
     try:
         nginx_conf_file = os.path.join(NGINX_HOME, 'sites-available', subdomain)
         click.echo(f'Copying Nginx config file: {nginx_conf_file}...')
-        shutil.copyfile('nginx.template.conf', nginx_conf_file)
+        os.system('sudo cp nginx.template.conf ' + nginx_conf_file)
         with open(nginx_conf_file, 'r') as f:
             data = f.read()
             data = data.replace('{{server_name}}', subdomain + '.' + domain)
